8edc221d50e9d3bfc4f00b7c4ef8a3551c54a681
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2026-03-23 13:05:16 +0200

    Removed useless events

--- TUSofiaFinalExam/src/shapes/Shape.cs
+++ TUSofiaFinalExam/src/shapes/Shape.cs
@@ -43,10 +43,6 @@ namespace TUSofiaFinalExam.Shapes
         public event Action<Shape> OnMoved;
         public event Action<Shape> OnResized;
         public event Action<Shape> OnColorChanged;
-        public event Action<Shape> OnClicked;
-        public event Action<Shape> OnDoubleClicked;
-        public event Action<Shape> OnMouseEnter;
-        public event Action<Shape> OnMouseLeave;
         
         public Shape(Point position, Color color)
         {
